763a5412f9f3b425a11828d7bf7f0454d4b7a628
CHaRiSMa72 <bilal.k.722@gmaiil.com>
2022-11-27 19:41:08 +0300

    React Native Chat App

--- App.js
+++ App.js
@@ -1,20 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { Text } from "react-native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import ChatList from "./screens/ChatList";
+import Settings from "./screens/Settings";
+import Chat from "./screens/Chat";
+import SignUp from "./screens/SignUp";
+import SignIn from "./screens/SignIn";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import { Provider, DefaultTheme } from "react-native-paper";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
-export default function App() {
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDfal7yZHqcpVBaYWPwbGLXguxyAYvMXA8",
+  authDomain: "reactnativechatapp-32f77.firebaseapp.com",
+  projectId: "reactnativechatapp-32f77",
+  storageBucket: "reactnativechatapp-32f77.appspot.com",
+  messagingSenderId: "917602789609",
+  appId: "1:917602789609:web:bcef0e9fea2f09ef4b6878"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const Stack = createNativeStackNavigator();
+
+const Tabs = createBottomTabNavigator();
+
+const TabsNavigator = () => {
+  const navigation = useNavigation();
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        navigation.navigate("SignUp");
+      }
+    });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          return (
+            <Ionicons
+              name={route.name === "ChatList" ? "chatbubbles" : "settings"}
+              color={color}
+              size={size}
+            />
+          );
+        },
+      })}
+    >
+      <Tabs.Screen name="ChatList" component={ChatList} />
+      <Tabs.Screen name="Settings" component={Settings} />
+    </Tabs.Navigator>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#2196f3",
+    accent: "#e91e63",
   },
-});
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Provider theme={theme}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Main"
+            component={TabsNavigator}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="Chat" component={Chat} />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUp}
+            options={{ presentation: "fullScreenModal" }}
+          />
+          <Stack.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{ presentation: "fullScreenModal" }}
+          />
+        </Stack.Navigator>
+      </Provider>
+    </NavigationContainer>
+  );
+};
+
+export default App;
